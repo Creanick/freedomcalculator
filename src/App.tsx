198901992ba59a03a657abe28ef8cf06e8808e47
creanick <creanickofficial@gmail.com>
2021-11-24 14:23:48 +0530

    homepage deleted

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <h1>Financial Freedom Calculator</h1>
-      <h2>Under Rapid Development</h2>
+      <h2>Coming soon</h2>
     </div>
   );
 }
