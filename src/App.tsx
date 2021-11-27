ec48f859731175808e9ad4efb906746f89fe7783
creanick <creanickofficial@gmail.com>
2021-11-27 12:27:38 +0530

    netlify auto deploy workflow created

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,7 @@ function App() {
   return (
     <div className="App">
       <h1>Financial Freedom Calculator</h1>
-      <h2>Coming Early...</h2>
-      <h2>Under Rapid Development</h2>
+      <h2>Fastest Development Running...</h2>
     </div>
   );
 }
