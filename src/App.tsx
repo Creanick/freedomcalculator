9d3c7de1ae4a74c79d38c7907700a8cb0ee07b9c
creanick <creanickofficial@gmail.com>
2021-11-27 11:03:00 +0530

    created redicret file

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <h1>Financial Freedom Calculator</h1>
       <h2>Coming soon...</h2>
-      <h2>Under Rapi Development</h2>
+      <h2>Under Rapid Development</h2>
     </div>
   );
 }
