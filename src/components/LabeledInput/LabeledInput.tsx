59515320f510af02e594180c8701f56975e7e04a
creanick <creanickofficial@gmail.com>
2021-12-02 17:18:15 +0530

    error label used in labeled input component

--- src/components/LabeledInput/LabeledInput.tsx
+++ src/components/LabeledInput/LabeledInput.tsx
@@ -1,5 +1,6 @@
 import { FunctionComponent } from 'react'
 import styled from 'styled-components'
+import ErrorLabel from '../ErrorLabel/ErrorLabel';
 
 const Wrapper = styled.div`
     text-align: left;
@@ -14,12 +15,15 @@ const Wrapper = styled.div`
 interface Props {
     inputElement: JSX.Element
     id: string
+    errorMessage?: string
+    showError?: boolean
 }
-const LabeledInput: FunctionComponent<Props> = ({ id, children, inputElement }) => {
+const LabeledInput: FunctionComponent<Props> = ({ id, children, inputElement, errorMessage, showError }) => {
     return (
         <Wrapper>
             <label htmlFor={id}>{children}</label>
             {inputElement}
+            <ErrorLabel message={errorMessage} show={showError} />
         </Wrapper>
     )
 }
