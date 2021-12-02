d91b9bbce281196feef01c2871ff41ce247ba539
creanick <creanickofficial@gmail.com>
2021-12-02 17:26:55 +0530

    form validation added

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
+import { FormikErrors, useFormik } from "formik";
 import { useState } from "react";
 import styled from "styled-components";
 import Button from "./components/Button/Button";
 import Input from "./components/Input/Input";
 import LabeledInput from "./components/LabeledInput/LabeledInput";
-import FreedomCalculatorEntity from "./domain/entity/freedom_calculator_entity";
 
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  h1{
+  .title{
     font-weight: 600;
     text-align: center;
   }
@@ -26,56 +26,102 @@ const InputContainer = styled.form`
   @media screen and (max-width:${inputContainerWidth}){
     width:100%;
   }
-  p{
-    font-weight: 600;
-    margin:0;
-    text-align: center;
-    &.total-fund{
+  .total-fund-container {
+    p{
+      font-weight: 600;
+      margin:0;
+      text-align: center;
+    }
+    p.total-fund{
       color: seagreen;
     }
   }
 `;
 const App = () => {
-  const [expense, setExpense] = useState("");
-  const [currentAge, setCurrentAge] = useState("");
-  const [freedomAge, setFreedomAge] = useState("");
-  const [lifeExpectancy, setLifeExpectancy] = useState("80");
-  const [inflationRate, setInflationRate] = useState("6");
-  const [postFreedomReturn, setPostFreedomReturn] = useState("10");
-  const [totalFund, setTotalFund] = useState(0);
-  const onCalculate = () => {
-    const expenseValue = Number(expense);
-    const currentAgeValue = Number(currentAge);
-    const freedomAgeValue = Number(freedomAge);
-    const lifeExpectancyValue = Number(lifeExpectancy);
-    const inflationRateValue = Number(inflationRate);
-    const postFreedomReturnValue = Number(postFreedomReturn);
-    if (isNaN(expenseValue) || isNaN(currentAgeValue) || isNaN(freedomAgeValue) || isNaN(lifeExpectancyValue) || isNaN(inflationRateValue) || isNaN(postFreedomReturnValue)) {
-      return;
+  const formik = useFormik({
+    initialValues: {
+      expense: "",
+      currentAge: "",
+      freedomAge: "",
+      lifeExpectancy: "80",
+      inflationRate: "6",
+      postFreedomReturn: "10"
+    },
+    validate: values => {
+      const errors: FormikErrors<{
+        expense: string;
+        currentAge: string;
+        freedomAge: string;
+        lifeExpectancy: string;
+        inflationRate: string;
+        postFreedomReturn: string;
+      }> = {};
+      const expenseValue = Number(values.expense);
+      const currentAgeValue = Number(values.currentAge);
+      const freedomAgeValue = Number(values.freedomAge);
+      const lifeExpectancyValue = Number(values.lifeExpectancy);
+      const inflationRateValue = Number(values.inflationRate);
+      const postFreedomReturnValue = Number(values.postFreedomReturn);
+      if (expenseValue < 0) {
+        errors.expense = "Expense must be greater than or equal to 0";
+      }
+      if (currentAgeValue < 1) {
+        errors.currentAge = "Current Age must be greater than 0";
+      } else if (currentAgeValue >= freedomAgeValue) {
+        errors.currentAge = "Current Age must be less than freedom age";
+      } else if (currentAgeValue >= lifeExpectancyValue) {
+        errors.currentAge = "Current Age must be less than life expentancy";
+      }
+      if (freedomAgeValue < 1) {
+        errors.freedomAge = "Freedom Age must be greater than 0";
+      } else if (freedomAgeValue <= currentAgeValue) {
+        errors.freedomAge = "Freedom Age must be greater than current age";
+      } else if (freedomAgeValue > lifeExpectancyValue) {
+        errors.freedomAge = "Freedom age must be less than or equal to life expectancy";
+      }
+      if (lifeExpectancyValue < 1) {
+        errors.lifeExpectancy = "Life Expentancy must be greater than 0";
+      } else if (lifeExpectancyValue <= currentAgeValue) {
+        errors.lifeExpectancy = "Life Expectancy must be greater than current age";
+      } else if (lifeExpectancyValue < freedomAgeValue) {
+        errors.lifeExpectancy = "Life Expectancy must greater than or equal to freedom age";
+      }
+      if (isNaN(inflationRateValue)) {
+        errors.inflationRate = "Inflaiton Rate must be number";
+      }
+      if (isNaN(postFreedomReturnValue)) {
+        errors.postFreedomReturn = "Post Freedom Return Rate must be number";
+      }
+      return errors;
+    },
+    onSubmit: values => {
+      console.log(values);
     }
-    const totalFundValue = FreedomCalculatorEntity.calculateTotalFund({
-      currentAge: currentAgeValue,
-      freedomAge: freedomAgeValue,
-      inflationRate: inflationRateValue,
-      lifeExpectancy: lifeExpectancyValue,
-      monthlyExpense: expenseValue,
-      postFreedomReturn: postFreedomReturnValue
-    });
-    setTotalFund(totalFundValue);
-  }
+  })
+  const { currentAge, expense, freedomAge, inflationRate, lifeExpectancy, postFreedomReturn } = formik.values;
+  const [totalFund, setTotalFund] = useState(0);
   return (
     <Wrapper>
-      <h1>Financial Freedom Calculator</h1>
-      <InputContainer onSubmit={e => { e.preventDefault(); onCalculate(); }}>
-        <LabeledInput id="expense" inputElement={<Input type="number" placeholder="Ex:50000" id="expense" value={expense} onChange={e => setExpense(e.target.value)} />}>Monthly Expense</LabeledInput>
-        <LabeledInput id="current-age" inputElement={<Input type="number" placeholder="Ex:20" id="current-age" value={currentAge} onChange={e => setCurrentAge(e.target.value)} />}>Current Age</LabeledInput>
-        <LabeledInput id="freedom-age" inputElement={<Input type="number" placeholder="Ex:40" id="freedom-age" value={freedomAge} onChange={e => setFreedomAge(e.target.value)} />}>Freedom Age</LabeledInput>
-        <LabeledInput id="life-expectancy" inputElement={<Input type="number" placeholder="Ex:80" id="life-expectancy" value={lifeExpectancy} onChange={e => setLifeExpectancy(e.target.value)} />}>Life Expectancy</LabeledInput>
-        <LabeledInput id="inflation-rate" inputElement={<Input type="number" placeholder="Ex:6%" id="inflation-rate" value={inflationRate} onChange={e => setInflationRate(e.target.value)} />}>Inflation Rate</LabeledInput>
-        <LabeledInput id="post-freedom-return" inputElement={<Input type="number" placeholder="Ex:10%" id="post-freedom-return" value={postFreedomReturn} onChange={e => setPostFreedomReturn(e.target.value)} />}>Post Freedom Return</LabeledInput>
-        <Button>Calculate</Button>
-        <p>Total Fund Needed</p>
-        <p className="total-fund">{totalFund}</p>
+      <h1 className="title">Financial Freedom Calculator</h1>
+      <InputContainer onSubmit={formik.handleSubmit}>
+
+        <LabeledInput id="expense" showError={formik.touched.expense} errorMessage={formik.errors.expense} inputElement={<Input type="number" onBlur={formik.handleBlur} placeholder="Ex:50000" id="expense" value={expense} onChange={formik.handleChange} />}>Monthly Expense</LabeledInput>
+
+        <LabeledInput id="currentAge" showError={formik.touched.currentAge} errorMessage={formik.errors.currentAge} inputElement={<Input type="number" onBlur={formik.handleBlur} placeholder="Ex:20" id="currentAge" value={currentAge} onChange={formik.handleChange} />}>Current Age</LabeledInput>
+
+        <LabeledInput id="freedomAge" showError={formik.touched.freedomAge} errorMessage={formik.errors.freedomAge} inputElement={<Input type="number" onBlur={formik.handleBlur} placeholder="Ex:40" id="freedomAge" value={freedomAge} onChange={formik.handleChange} />}>Freedom Age</LabeledInput>
+
+        <LabeledInput id="lifeExpectancy" showError={formik.touched.lifeExpectancy} errorMessage={formik.errors.lifeExpectancy} inputElement={<Input type="number" onBlur={formik.handleBlur} placeholder="Ex:80" id="lifeExpectancy" value={lifeExpectancy} onChange={formik.handleChange} />}>Life Expectancy</LabeledInput>
+
+        <LabeledInput id="inflationRate" showError={formik.touched.inflationRate} errorMessage={formik.errors.inflationRate} inputElement={<Input type="number" onBlur={formik.handleBlur} placeholder="Ex:6%" id="inflationRate" value={inflationRate} onChange={formik.handleChange} />}>Inflation Rate</LabeledInput>
+
+        <LabeledInput id="postFreedomReturn" showError={formik.touched.postFreedomReturn} errorMessage={formik.errors.postFreedomReturn} inputElement={<Input type="number" onBlur={formik.handleBlur} placeholder="Ex:10%" id="postFreedomReturn" value={postFreedomReturn} onChange={formik.handleChange} />}>Post Freedom Return</LabeledInput>
+
+        <Button type="submit">Calculate</Button>
+        <div className="total-fund-container">
+          <p>Total Fund Needed</p>
+          <p className="total-fund">{totalFund}</p>
+        </div>
       </InputContainer>
     </Wrapper>
   );
