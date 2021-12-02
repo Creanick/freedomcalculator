018b8dace386fa89a27771c413c62b67b822694d
creanick <creanickofficial@gmail.com>
2021-12-02 12:57:40 +0530

    change input focus color

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -4,9 +4,13 @@ const Input = styled.input`
     display: block;
     box-sizing: border-box;
     width: 100%;
-    border: 1px solid grey;
+    border: 1px solid #cdcdcd;
     padding: 12px 16px;
     border-radius: 4px;
+    &:focus{
+        outline:none;
+        border: 1px solid dodgerblue;
+    }
 `;
 
 export default Input
