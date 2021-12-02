31feff6af57b2df0998c0d9e1c83c04ec2eeb5ec
creanick <creanickofficial@gmail.com>
2021-12-02 12:50:54 +0530

    basic button compoennt created

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+const Button = styled.button`
+    padding: 12px 18px;
+    border: none;
+    font-weight: bold;
+    border-radius: 4px;
+    background-color: dodgerblue;
+    color:white;
+    cursor: pointer;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    &:hover{
+        background-color: #0281ff;
+    }
+    &:active{
+        transform: scale(0.98);
+    }
+`;
+
+export default Button
