3638a666314166ef808dd757a1ecb86a12fc1af4
creanick <creanickofficial@gmail.com>
2021-12-11 16:11:34 +0530

    removed show error border red from custom input component

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -3,12 +3,12 @@ const Input = styled.input<{ error?: boolean }>`
     display: block;
     box-sizing: border-box;
     width: 100%;
-    border: 1px solid ${props => props.error ? "red" : "#cdcdcd"};
+    border: 1px solid #cdcdcd;
     padding: 12px 16px;
     border-radius: 4px;
     &:focus{
         outline:none;
-        border: 1px solid ${props => props.error ? "red" : "dodgerblue"};
+        border: 1px solid dodgerblue;
     }
 `;
 
