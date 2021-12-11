8dc1a3a18093312ddcbe6e19858355224fa7ab88
creanick <creanickofficial@gmail.com>
2021-12-11 12:22:55 +0530

    added red border on error

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,11 +1,16 @@
-import styled from 'styled-components'
-const Input = styled.input`
+import styled, { css } from 'styled-components'
+const Input = styled.input<{ error?: boolean }>`
     display: block;
     box-sizing: border-box;
     width: 100%;
     border: 1px solid #cdcdcd;
     padding: 12px 16px;
     border-radius: 4px;
+    ${props => props.error && css`
+        &,&:focus{
+            border-color:red;
+        }
+    `}
     &:focus{
         outline:none;
         border: 1px solid dodgerblue;
