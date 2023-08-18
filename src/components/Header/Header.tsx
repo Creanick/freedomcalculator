276fc550ff69287ed5a44da2e8fbcdc5db3cde8c
Manick <manickware@gmail.com>
2023-08-18 13:56:35 +0530

    new logo added

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,45 +1,43 @@
-import styled from 'styled-components';
-import logo from '../../assets/logo.svg';
-const Logo = styled.img`
-`;
+import styled from "styled-components";
+import logo from "../../assets/logo.svg";
+const Logo = styled.img``;
 const Wrapper = styled.header`
-    /* border: 1px solid black; */
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    text-align: center;
-    padding: 20px 30px 10px;
-    @media screen and (min-width: 350px){
-        flex-direction: row;
-        text-align: left;
-        ${Logo}{
-            margin-right: 20px;
-        }
+  /* border: 1px solid black; */
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  padding: 20px 30px 10px;
+  @media screen and (min-width: 350px) {
+    flex-direction: row;
+    text-align: left;
+    ${Logo} {
+      margin-right: 20px;
     }
+  }
 `;
-const NameSection = styled.section`
-`;
+const NameSection = styled.section``;
 const SubTitle = styled.h2`
-    font-size: 12px;
-    font-weight: 600;
-    margin: 0;
-    color: grey;
+  font-size: 12px;
+  font-weight: 600;
+  margin: 0;
+  color: grey;
 `;
 const Title = styled.h1`
-    margin:0;
-    font-size: 34px;
-    color: #464646;
+  margin: 0;
+  font-size: 34px;
+  color: #464646;
 `;
 const Header = () => {
-    return (
-        <Wrapper>
-            <Logo src={logo} alt="" width={60} height={60} />
-            <NameSection>
-                <Title>FFC</Title>
-                <SubTitle>Financial Freedom Calculator</SubTitle>
-            </NameSection>
-        </Wrapper>
-    )
-}
+  return (
+    <Wrapper>
+      <Logo src={logo} alt="" width={60} height={60} />
+      <NameSection>
+        <Title>Freedom Calculator</Title>
+        <SubTitle>Financial Freedom Calculator</SubTitle>
+      </NameSection>
+    </Wrapper>
+  );
+};
 
-export default Header
+export default Header;
