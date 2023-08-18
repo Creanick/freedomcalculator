5a3ab7d9ff2f12d98b6879bd5339c0f0f047d3cd
Manick <manickware@gmail.com>
2023-08-18 13:29:05 +0530

    usd inr currency formatter added

--- src/components/FreedomFundResultCard/FreedomFundResultCard.tsx
+++ src/components/FreedomFundResultCard/FreedomFundResultCard.tsx
@@ -1,34 +1,80 @@
-import React from 'react'
-import styled from 'styled-components';
-import Fund, { FundPriority } from '../Fund/Fund';
+import React, { useState } from "react";
+import styled from "styled-components";
+import Fund, { FundPriority } from "../Fund/Fund";
 const Wrapper = styled.div`
-    border-radius: 4px;
-    background-color: white;
-    box-shadow: 12px 13px 39px -7px #0000001c;
-    padding: 40px;
-    display: grid;
-    gap: 16px;
-    max-width: 380px;
-    box-sizing: border-box;
-    overflow: auto;
+  border-radius: 4px;
+  background-color: white;
+  box-shadow: 12px 13px 39px -7px #0000001c;
+  padding: 40px;
+  display: grid;
+  gap: 16px;
+  max-width: 380px;
+  box-sizing: border-box;
+  overflow: auto;
+  justify-content: center;
+`;
+const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+const CurrencyButton = styled.div<{ active?: boolean }>`
+  border: none;
+  padding: 4px 12px;
+  border: 1px solid #1e90ff;
+  background-color: ${(props) => (props.active ? "#1E90FF" : "transparent")};
+  color: ${(props) => (props.active ? "#ffffff" : "#767676")};
+  cursor: pointer;
 `;
 interface Props {
-    monthlyExpenseAtFreedom: number,
-    fundValues: {
-        mortalFund: number,
-        immortalFund: number,
-    },
-    freedomAge: number,
-    lifeExpectancy: number
+  monthlyExpenseAtFreedom: number;
+  fundValues: {
+    mortalFund: number;
+    immortalFund: number;
+  };
+  freedomAge: number;
+  lifeExpectancy: number;
 }
-const FreedomFundResultCard = React.forwardRef<any, Props>(({ freedomAge, fundValues, lifeExpectancy, monthlyExpenseAtFreedom }, ref) => {
+const FreedomFundResultCard = React.forwardRef<any, Props>(
+  (
+    { freedomAge, fundValues, lifeExpectancy, monthlyExpenseAtFreedom },
+    ref
+  ) => {
+    const [currency, setCurrency] = useState<"USD" | "INR">("USD");
     return (
-        <Wrapper ref={ref as any}>
-            <Fund priority={FundPriority.primary} amount={fundValues.mortalFund}>Total Fund Needed At {freedomAge} years age for next {lifeExpectancy - freedomAge} years</Fund>
-            <Fund amount={monthlyExpenseAtFreedom}>Monthly expense at {freedomAge} years age</Fund>
-            <Fund amount={fundValues.immortalFund}>Total fund needed At {freedomAge} years age to use forever</Fund>
-        </Wrapper>
-    )
-})
+      <Wrapper ref={ref as any}>
+        <Fund
+          priority={FundPriority.primary}
+          amount={fundValues.mortalFund}
+          currency={currency}
+        >
+          Total fund needed at {freedomAge} years age for next{" "}
+          {lifeExpectancy - freedomAge} years
+        </Fund>
+        <Fund amount={monthlyExpenseAtFreedom} currency={currency}>
+          Monthly expense at {freedomAge} years age
+        </Fund>
+        <Fund amount={fundValues.immortalFund} currency={currency}>
+          Total fund needed At {freedomAge} years age to use forever
+        </Fund>
+        <ButtonWrapper>
+          <CurrencyButton
+            active={currency === "USD"}
+            style={{ borderRadius: "4px 0px 0px 4px" }}
+            onClick={() => setCurrency("USD")}
+          >
+            USD
+          </CurrencyButton>
+          <CurrencyButton
+            active={currency === "INR"}
+            style={{ borderRadius: "0px 4px 4px 0px" }}
+            onClick={() => setCurrency("INR")}
+          >
+            INR
+          </CurrencyButton>
+        </ButtonWrapper>
+      </Wrapper>
+    );
+  }
+);
 
-export default FreedomFundResultCard
+export default FreedomFundResultCard;
--- src/components/Fund/Fund.tsx
+++ src/components/Fund/Fund.tsx
@@ -1,49 +1,59 @@
-import { FunctionComponent } from 'react'
-import styled, { css } from 'styled-components';
+import { FunctionComponent } from "react";
+import styled, { css } from "styled-components";
+import formatCurrency from "../../utils/formatCurrency";
 export enum FundPriority {
-    primary,
-    secondary
+  primary,
+  secondary,
 }
 interface Props {
-    amount: number
-    priority?: FundPriority
+  amount: number;
+  priority?: FundPriority;
+  currency?: "USD" | "INR";
 }
 const titleBaseColor = "#858585";
 const amountBaseColor = "#4c4c4c";
 const FundTitle = styled.p`
-    font-size: 14px;
-    font-weight: 500;
-    `;
-const FundAmount = styled.p`
-    font-size: 26px;
-    font-weight: 600;
-    `;
-const Wrapper = styled.div<{ active?: boolean }>`
-    ${FundTitle},${FundAmount}{
-        margin:0;
-    }
-    ${FundAmount}{
-        color: ${amountBaseColor};
-    }
-    ${FundTitle}{
-        color:${titleBaseColor};
-        margin-bottom: 4px;
-    }
-    ${props => {
-        return props.active && css`
-            ${FundAmount}{
-                color: seagreen;
-            }
-        `;
-    }}
+  font-size: 14px;
+  font-weight: 500;
 `;
-const Fund: FunctionComponent<Props> = ({ children, amount, priority = FundPriority.secondary }) => {
+const FundAmount = styled.p`
+  font-size: 26px;
+  font-weight: 600;
+`;
+const Wrapper = styled.div<{ active?: boolean }>`
+  ${FundTitle},${FundAmount} {
+    margin: 0;
+  }
+  ${FundAmount} {
+    color: ${amountBaseColor};
+  }
+  ${FundTitle} {
+    color: ${titleBaseColor};
+    margin-bottom: 4px;
+  }
+  ${(props) => {
     return (
-        <Wrapper active={priority === FundPriority.primary ? true : false}>
-            <FundTitle>{children}</FundTitle>
-            <FundAmount>{amount}</FundAmount>
-        </Wrapper>
-    )
-}
+      props.active &&
+      css`
+        ${FundAmount} {
+          color: seagreen;
+        }
+      `
+    );
+  }}
+`;
+const Fund: FunctionComponent<Props> = ({
+  children,
+  amount,
+  priority = FundPriority.secondary,
+  currency,
+}) => {
+  return (
+    <Wrapper active={priority === FundPriority.primary ? true : false}>
+      <FundTitle>{children}</FundTitle>
+      <FundAmount>{formatCurrency(amount, currency)}</FundAmount>
+    </Wrapper>
+  );
+};
 
-export default Fund
+export default Fund;
